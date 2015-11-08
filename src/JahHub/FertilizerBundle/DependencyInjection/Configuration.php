123eda89b5f1e6011b4c0db4d085c63eea996bc8
JahHub <jahhub@outlook.com>
2015-11-08 12:42:35 +0100

    fix insight issue

--- src/JahHub/FertilizerBundle/DependencyInjection/Configuration.php
+++ src/JahHub/FertilizerBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        //$rootNode = $treeBuilder->root('jahhub_fertilizer');
 
         return $treeBuilder;
     }
