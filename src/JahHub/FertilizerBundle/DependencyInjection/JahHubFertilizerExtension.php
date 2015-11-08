f4636557ff8d2f25b3c0df7c78fd0c53ff91d825
JahHub <jahhub@outlook.com>
2015-11-08 12:37:30 +0100

    fix insight issues

--- src/JahHub/FertilizerBundle/DependencyInjection/JahHubFertilizerExtension.php
+++ src/JahHub/FertilizerBundle/DependencyInjection/JahHubFertilizerExtension.php
@@ -2,9 +2,7 @@
 
 namespace JahHub\FertilizerBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
