0da911263b02e80243b5e9af79cc7ef679191cd6
JahHub <jahhub@outlook.com>
2015-11-11 22:24:41 +0100

    Repository factorization

--- src/JahHub/FertilizerBundle/Repository/StateRepository.php
+++ src/JahHub/FertilizerBundle/Repository/StateRepository.php
@@ -1,65 +1,9 @@
 <?php
 namespace JahHub\FertilizerBundle\Repository;
 
-use Doctrine\ORM\EntityRepository;
-
 /**
  * Class StateRepository
  */
-class StateRepository extends EntityRepository implements EntityRepositoryInterface
+class StateRepository extends AbstractRepository
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function create()
-    {
-        $className = $this->getClassName();
-
-        return new $className();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function exist($id)
-    {
-        $qb = $this->createQueryBuilder('state');
-        $qb->select('state.id')
-            ->where('state.id = :state_id')
-            ->setParameter('state_id', $id);
-
-        $result = $qb->getQuery()->getArrayResult();
-
-        return count($result) > 0;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function delete($id)
-    {
-        $qb = $this->createQueryBuilder('state');
-        $qb->delete($this->getClassName(), 'state')
-            ->where('state.id = :state_id')
-            ->setParameter('state_id', $id);
-
-        $qb->getQuery()->execute();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function all($page = 1, $limit = 5, $orderBy = null)
-    {
-        if ($limit < 1) {
-            $limit = 1;
-        }
-        if ($page < 1) {
-            $offset = 0;
-        } else {
-            $offset = ($page - 1) * $limit;
-        }
-
-        return $this->findBy(array(), $orderBy, $limit, $offset);
-    }
 }
